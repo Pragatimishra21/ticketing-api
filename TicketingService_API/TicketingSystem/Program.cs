5ea025e6220c57b1ebe79887360e6d90612f8939
pragatimishra3 <pragati.mishra@deltax.com>
2025-09-28 18:45:24 +0530

    added API changes

--- TicketingService_API/TicketingSystem/Program.cs
+++ TicketingService_API/TicketingSystem/Program.cs
@@ -30,7 +30,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("https://576pjz.csb.app")
+        policy.AllowAnyOrigin()
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
